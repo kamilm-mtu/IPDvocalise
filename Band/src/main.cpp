4cc0923da23a978a15ff62305ea181a288d1b526
kamilm-mtu <kamil.mandziuk@mycit.ie>
2023-02-02 11:12:40 +0000

    read microphone

--- Band/src/main.cpp
+++ Band/src/main.cpp
@@ -1,36 +1,27 @@
 #include <Arduino.h>
-#include <BluetoothA2DPSink.h>
+#include "BluetoothA2DPSink.h"
 
 BluetoothA2DPSink a2dp_sink;
-int micRead = 0;
 
- void data_received_callback() {
-  Serial.println("Data packet received");
-  }
-
-void setup() {
-  pinMode(26, OUTPUT);
-
-  static const i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN),
-        .sample_rate = 44100, // corrected by info from bluetooth
-        .bits_per_sample = (i2s_bits_per_sample_t) 16, /* the DAC module will only take the 8bits from MSB */
-        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-        .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_STAND_MSB,
-        .intr_alloc_flags = 0, // default interrupt priority
-        .dma_buf_count = 8,
-        .dma_buf_len = 64,
-        .use_apll = false
-    };
-
-    a2dp_sink.set_on_data_received(data_received_callback);
-    a2dp_sink.set_i2s_config(i2s_config);
-    a2dp_sink.start("MyMusic");
+void read_data_stream(const uint8_t *data, uint32_t length) {
+    int16_t *values = (int16_t*) data;
+    for (int j=0; j<length/2; j+=2){
+      // print the 2 channel values
+      Serial.print(values[j]);
+      Serial.print(",");
+      Serial.println(values[j+1]);
+    }
 }
 
+void setup() {
+  Serial.begin(115200);
+  // output to callback and no I2S 
+  a2dp_sink.set_stream_reader(read_data_stream, false);
+  // connect to MyMusic with no automatic reconnect
+  a2dp_sink.start("MyMusic", false);  
+}
+
+
 void loop() {
-  micRead = analogRead(26);
-  if (micRead > 0)
-    digitalWrite(26, HIGH);
-  data_received_callback;
+  delay(1000);
 }
--- Microphone/src/main.cpp
+++ Microphone/src/main.cpp
@@ -1,24 +1,67 @@
-#include <Arduino.h>
-#include <BluetoothA2DPSource.h>
-#include <AudioTools.h>
+#include "BluetoothA2DPSource.h"
+#include "Arduino.h"
+#include "AudioTools.h"
+#include <math.h>
+ 
+//#define c3_frequency  130.81
+//#define PI 3.14159265358979323846
 
-BluetoothA2DPSource a2dp_source;
+//BluetoothA2DPSource a2dp_source;
 
-const int micPin = 35;
-int micBuffer = 0;
+// The supported audio codec in ESP32 A2DP is SBC. SBC audio stream is encoded
+// from PCM data normally formatted as 44.1kHz sampling rate, two-channel 16-bit sample data
+//int32_t get_data_channels(Frame *frame, int32_t channel_len) {
+//    static double m_time = 0.0;
+//    double m_amplitude = 10000.0;  // -32,768 to 32,767
+//    double m_deltaTime = 1.0 / 44100.0;
+//    double m_phase = 0.0;
+//    double double_Pi = PI * 2.0;
+    // fill the channel data
+//    for (int sample = 0; sample < channel_len; ++sample) {
+//        double angle = double_Pi * c3_frequency * m_time + m_phase;
+//        frame[sample].channel1 = m_amplitude * sin(angle);
+//        frame[sample].channel2 = frame[sample].channel1;
+//        m_time += m_deltaTime;
+//    }
 
-int32_t get_sound_data(Frame *data, int32_t frameCount) {
-    // generate your sound data 
-    // return the effective length (in frames) of the generated sound  (which usually is identical with the requested len)
-    // 1 frame is 2 channels * 2 bytes = 4 bytes
-    frameCount = micBuffer;
-    return frameCount;
-}
+//    return channel_len;
+//}
 
-void setup() {
-  Serial.begin(9600);
-  a2dp_source.start("MyMusic", get_sound_data);
+
+//void setup() {
+  //a2dp_source.set_auto_reconnect(false);
+//  a2dp_source.start("MyMusic", get_data_channels);  
+//  a2dp_source.set_volume(30);
+//}
+
+//void loop() {
+  // to prevent watchdog in release > 1.0.6
+//  delay(10);
+//}
+
+#include "Arduino.h"
+#include "AudioTools.h"
+
+const uint16_t sample_rate = 8000;
+AnalogAudioStream in; 
+int channels = 1;
+
+CsvStream<int16_t> out(Serial, channels); // ASCII out
+StreamCopy copier(out, in); // Microphone -> ASCII out
+ConverterAutoCenter<int16_t> center(2); // set avg to 0
+
+void setup(void) {
+  Serial.begin(115200);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
+  // GP34 Default Output
+  auto cfgRx = in.defaultConfig(RX_MODE);
+  cfgRx.sample_rate = sample_rate;
+  cfgRx.channels = channels;
+  in.begin(cfgRx);
+  out.begin();
 }
 
 void loop() {
+  copier.copy(center);
 }
