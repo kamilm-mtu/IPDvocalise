76fcdf86ef382c50f64a32f05a5f2aac2b61bcfc
kamilm-mtu <kamil.mandziuk@mycit.ie>
2023-02-02 12:32:57 +0000

    Change to AudioTool lib

--- Band/src/main.cpp
+++ Band/src/main.cpp
@@ -1,27 +1,20 @@
 #include <Arduino.h>
-#include "BluetoothA2DPSink.h"
+#include "AudioTools.h"
+#include "AudioLibs/AudioA2DP.h"
 
-BluetoothA2DPSink a2dp_sink;
-
-void read_data_stream(const uint8_t *data, uint32_t length) {
-    int16_t *values = (int16_t*) data;
-    for (int j=0; j<length/2; j+=2){
-      // print the 2 channel values
-      Serial.print(values[j]);
-      Serial.print(",");
-      Serial.println(values[j+1]);
-    }
-}
+A2DPStream btin;
 
 void setup() {
-  Serial.begin(115200);
-  // output to callback and no I2S 
-  a2dp_sink.set_stream_reader(read_data_stream, false);
-  // connect to MyMusic with no automatic reconnect
-  a2dp_sink.start("MyMusic", false);  
+  Serial.begin(9600);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
+
+  auto cfgA2DP = btin.defaultConfig(RX_MODE);
+  cfgA2DP.name = "Vocalise";
+  cfgA2DP.auto_reconnect = true;
+  btin.begin(cfgA2DP);
+  Serial.println("Connected");
 }
 
-
 void loop() {
-  delay(1000);
+
 }
