315ca2d3aeaa1a92a9e1b42b83b41fc50e3299d6
kamilm-mtu <kamil.mandziuk@mycit.ie>
2023-02-16 17:20:18 +0000

    FFT start

--- Band/src/main.cpp
+++ Band/src/main.cpp
@@ -1,8 +1,32 @@
 #include <Arduino.h>
 #include "AudioTools.h"
 #include "AudioLibs/AudioA2DP.h"
+#include "AudioLibs/AudioRealFFT.h"
+
+const uint8_t channels = 2;
+const uint16_t sample_rate = 44100;
 
 A2DPStream btin;
+AudioRealFFT fft;
+StreamCopy copier(fft, btin);
+
+void writeDataStream(const uint8_t *data, uint32_t length) {
+  fft.write(data, length);
+}
+
+void fftResult(AudioFFTBase &fft) {
+  float diff;
+  auto result = fft.result();
+  if (result.magnitude>100){
+    Serial.print(result.frequency);
+    Serial.print(" ");
+    Serial.print(result.magnitude);
+    Serial.print(" => ");
+    Serial.print(result.frequencyAsNote(diff));
+    Serial.print("diff:");
+    Serial.println(diff);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -12,9 +36,16 @@ void setup() {
   cfgA2DP.name = "Vocalise";
   cfgA2DP.auto_reconnect = true;
   btin.begin(cfgA2DP);
-  Serial.println("Connected");
+
+  auto tcfg = fft.defaultConfig();
+  tcfg.length = 4096;
+  tcfg.channels = channels;
+  tcfg.sample_rate = sample_rate;
+  tcfg.bits_per_sample = 16;
+  tcfg.callback = &fftResult;
+  fft.begin(tcfg);
 }
 
 void loop() {
-
+  copier.copy();
 }
